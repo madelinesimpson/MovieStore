827e9654a4660b47bdd799ace42e9feca580f289
madelinesimpson <madelinegrsimpson@gmail.com>
2025-02-02 12:17:24 -0500

    Movies done

--- moviesstore/urls.py
+++ moviesstore/urls.py
@@ -18,11 +18,14 @@ from django.urls import path
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
+    path('movies/', include('movies.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL,
