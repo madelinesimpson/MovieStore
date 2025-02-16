e08528899ec840d493b02a63ed62bf5959216f88
sthompson322 <151972077+sthompson322@users.noreply.github.com>
2025-02-16 16:05:10 -0500

    added security question

--- accounts/models.py
+++ accounts/models.py
@@ -1,1 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class SecurityQuestion(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    securityAnswer = models.CharField(max_length=255, null=True, blank=True)
+    def __str__(self):
+        return str(self.id) + ' - ' + self.securityAnswer
